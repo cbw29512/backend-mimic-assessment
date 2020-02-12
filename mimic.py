a7079a3bfcf3af5c70e84b6d8f89fa111d377300
Chris Wilson <divclass01@gmail.com>
2020-02-12 11:59:08 -0500

    works as intended

--- mimic.py
+++ mimic.py
@@ -46,49 +46,55 @@ columns, so the output looks better.
 import random
 import sys
 
-__author__ = "???"
 
-
-def create_mimic_dict(filename):
-    """Returns mimic dict mapping each word to list of words which follow it.
-    For example:
-        Input: "I am a software developer, and I don't care who knows"
-        Output:
-            {
-                "" : ["I"],
-                "I" : ["am", "don't"],
-                "am": ["a"],
-                "a": ["software"],
-                "software" : ["developer,"],
-                "developer," : ["and"],
-                "and" : ["I"],
-                "don't" : ["care"],
-                "care" : ["who"],
-                "who" : ["knows"]
-            }
-    """
+def mimic_dict(filename):
+    """Returns mimic dict mapping each word to list of words which follow it."""
     # +++your code here+++
+    # LAB(begin solution)
+    mimic_dict = {}
+    f = open(filename, 'r')
+    text = f.read()
+    f.close()
+    words = text.split()
+    prev = ''
+    for word in words:
+        if not prev in mimic_dict:
+            mimic_dict[prev] = [word]
+        else:
+            mimic_dict[prev].append(word)
+        # Could write as: mimic_dict[prev] = mimic_dict.get(prev, []) + [word]
+        # It's one line, but not totally satisfying.
+        prev = word
+    return mimic_dict
+    # LAB(replace solution)
+    # return
+    # LAB(end solution)
 
 
-def print_mimic(mimic_dict, start_word):
-    """Given a previously compiled mimic_dict and start_word, prints 200 random words:
-        - Print the start_word
-        - Lookup the start_word in your mimic_dict and get it's next-list
-        - Randomly select a new word from the next-list
-        - Repeat this process 200 times
-    """
+def print_mimic(mimic_dict, word):
+    """Given mimic dict and start word, prints 200 random words."""
     # +++your code here+++
-    pass
+    # LAB(begin solution)
+    for unused_i in range(200):
+        print word,
+        nexts = mimic_dict.get(word)          # Returns None if not found
+        if not nexts:
+            nexts = mimic_dict['']  # Fallback to '' if not found
+        word = random.choice(nexts)
+    # The 'unused_' prefix turns off the lint warning about the unused variable.
+    # LAB(replace solution)
+    # return
+    # LAB(end solution)
 
 
 # Provided main(), calls mimic_dict() and mimic()
 def main():
     if len(sys.argv) != 2:
-        print 'usage: python mimic.py file-to-read'
+        print 'usage: ./mimic.py file-to-read'
         sys.exit(1)
 
-    d = create_mimic_dict(sys.argv[1])
-    print_mimic(d, '')
+    dict = mimic_dict(sys.argv[1])
+    print_mimic(dict, '')
 
 
 if __name__ == '__main__':
